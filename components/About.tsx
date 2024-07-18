0e6adc8d4947cfbbb2e00a6a3234d108cbcb629d
rithiksai27 <rithiksai27@gmail.com>
2024-07-18 15:17:30 +0530

    first commit

---
+++ components/About.tsx
@@ -0,0 +1,34 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { useState } from "react";
+import { IoCopyOutline } from "react-icons/io5";
+import Lottie from "react-lottie";
+
+import { cn } from "../utils/cn";
+import { BackgroundGradientAnimation } from "../components/ui/GradientBg";
+import GridGlobe from "../components/ui/GridGlobe";
+import animationData from "@/data/confetti.json";
+import MagicButton from "../components/ui/MagicButton";
+const About = () => {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default About;
